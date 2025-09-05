04ff7fedabf143fd4bdb769116761b63ea347a13
shashank <111701921+shawshanks666@users.noreply.github.com>
2025-09-05 14:42:08 +0530

    added sample header

--- speech-master/src/App.js
+++ speech-master/src/App.js
@@ -24,6 +24,7 @@ const App = () => {
         <>
             <div className="container">
                 <h2>Speech to Text Converter</h2>
+                <h2>...Sample Text...</h2>
                 <br/>
                 <p>A React hook that converts speech from the microphone to text and makes it available to your React
                     components.</p>
