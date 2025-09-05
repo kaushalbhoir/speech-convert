ed8b767c6bcc684c8325bf7a21fbabd6a9bd020c
kaushalbhoir <114304583+kaushalbhoir@users.noreply.github.com>
2025-09-05 15:39:19 +0530

    "last edited"

--- speech-master/src/App.js
+++ speech-master/src/App.js
@@ -30,7 +30,7 @@ const App = () => {
                 <h2>2nd try</h2>
                 <h2>Speech to Text Converter.Converts to text</h2>
                 <h2>...Sample Text...</h2>
->>>>>>> 5ebf5c8e3615fe206c0ceefc39fbf05fe55271af
+
                 <br/>
                 <p>A React hook that converts speech from the microphone to text and makes it available to your React components.</p>
 
