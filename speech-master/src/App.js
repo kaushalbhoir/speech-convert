4091e664658f084d953fb9780a510ffe698c1293
kaushalbhoir <114304583+kaushalbhoir@users.noreply.github.com>
2025-09-05 15:20:00 +0530

    "2nd try "

--- speech-master/src/App.js
+++ speech-master/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
     return (
         <>
             <div className="container">
-                <h2>Try Speech Converter</h2>
+                <h2>2nd try</h2>
                 <h2>Speech to Text Converter.Converts to text</h2>
                 <h2>...Sample Text...</h2>
                 <br/>
